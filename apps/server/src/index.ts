0ac47b14a266abff98c62b30808d4a4adbc10bfd
Alexander Gerasimov <2632391-Arctomachine@users.noreply.gitlab.com>
2024-05-23 23:13:54 +0300

    websocket на сервере

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,18 +1,16 @@
-import * as http from 'node:http'
 import * as crypto from 'node:crypto'
+import * as http from 'node:http'
+import { WebSocket, WebSocketServer } from 'ws'
 
 const messages: Message[] = []
-
-for (let i = 0; i < 15; i++) {
-	onNewMessage(`hello ${i}`)
-}
+const webSocketClients: WebSocket[] = []
 
 type Message = {
 	id: string
 	text: string
 }
 
-function onNewMessage (text: string) {
+function onNewMessage(text: string) {
 	const message: Message = {
 		id: crypto.randomUUID(),
 		text: text,
@@ -23,11 +21,23 @@ function onNewMessage (text: string) {
 	while (messages.length > 9) {
 		messages.shift()
 	}
+
+	for (const ws of webSocketClients) {
+		if (ws.readyState === WebSocket.OPEN) {
+			ws.send(
+				JSON.stringify({
+					lastMessageId: message.id,
+				}),
+			)
+		}
+	}
 }
 
 const server = http.createServer((req, res) => {
 	if (req.method === 'GET' && req.url === '/messages') {
-		res.writeHead(200, { 'Content-Type': 'application/json' })
+		res.writeHead(200, {
+			'Content-Type': 'application/json',
+		})
 		res.end(JSON.stringify({ data: messages }))
 		return
 	}
@@ -45,7 +55,9 @@ const server = http.createServer((req, res) => {
 
 			onNewMessage(message.text)
 
-			res.writeHead(201, { 'Content-Type': 'application/json' })
+			res.writeHead(201, {
+				'Content-Type': 'application/json',
+			})
 			res.end(JSON.stringify({ message: 'ok' }))
 		})
 		return
@@ -53,6 +65,17 @@ const server = http.createServer((req, res) => {
 
 	res.writeHead(404, { 'Content-Type': 'text/plain' })
 	res.end('Not Found')
+})
+
+const wss = new WebSocketServer({
+	port: 3211,
+	path: '/ws',
+})
+
+wss.on('connection', function connection(ws) {
+	ws.on('error', console.error)
+
+	webSocketClients.push(ws)
 })
 
 const PORT = 3210
