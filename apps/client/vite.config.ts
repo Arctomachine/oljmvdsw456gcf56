cc331efd8f82a881ce3bda5f2c5a348c5e102cda
Alexander Gerasimov <2632391-Arctomachine@users.noreply.gitlab.com>
2024-05-23 23:09:31 +0300

    прокси для cors

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server:{
+    proxy:{
+      '/messages': 'http://localhost:3210',
+      '/ws': {
+        target: 'http://localhost:3211',
+        ws: true
+      }
+    },
+  }
 })
